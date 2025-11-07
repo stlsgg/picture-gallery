fc19cf93d9c2dd67bcb9a14c67d5b780cb946da8
stlsgg <gregory.guzey@gmail.com>
2025-11-07 20:23:00 +0300

    fix: корректная проверка на дубликат изображения

--- src/index.php
+++ src/index.php
@@ -91,7 +91,7 @@ if (filesize("/data/meta.json") === 0) {
 
 // O(n) проход по ключам, ищем по каждому id файл
 foreach ($metadata as $id => $pic_info) {
-  if (strpos($pic_info["full"], $file_name)) {
+  if (strpos($pic_info["full"], $file_name) !== false) {
     http_response_code(400);
     echo "dublicate image founded in storage\n";
     exit; // если нашли дубликат, выходим из скрипта
