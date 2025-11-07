f4fe70c86c4afb47341df24e5d218a48b13c995f
stlsgg <gregory.guzey@gmail.com>
2025-11-07 23:42:50 +0300

    feat: drap-and-drop file form

---
+++ src/js/main.js
@@ -0,0 +1,34 @@
+// drag and drop feature
+const drop_zone = document.getElementById("drop-zone");
+const file_input = document.getElementById("file-upload");
+
+drop_zone.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  drop_zone.classList.add("active");
+});
+
+drop_zone.addEventListener("dragleave", (e) => {
+  e.preventDefault();
+  drop_zone.classList.remove("active");
+});
+
+drop_zone.addEventListener("drop", (e) => {
+  e.preventDefault();
+  drop_zone.classList.remove("active");
+
+  const file = e.dataTransfer.files[0];
+  file_input.files = e.dataTransfer.files;
+
+  const label = drop_zone.querySelector("label");
+  label.innerText = file.name;
+});
+
+file_input.addEventListener("change", () => {
+  if (file_input.files.length > 0) {
+    const label = drop_zone.querySelector("label");
+    label.innerText = file_input.files[0].name;
+  } else {
+    const label = drop_zone.querySelector("label");
+    label.innerText = "Перетащите сюда картинку :3";
+  }
+});
