c959d7d4e5a5bc4f075f56e684ee9b1f588fcd90
stlsgg <gregory.guzey@gmail.com>
2025-11-08 12:32:39 +0300

    feat: /api/query.php
    создан файл-обработчик вызовов api

--- src/api/query.php
+++ src/api/query.php
@@ -1,2 +1,19 @@
 <?php
-echo "hello";
+header("Content-Type: text/plain");
+$uri = $_SERVER["REQUEST_URI"];
+$uri = rtrim($uri, " /");
+echo "$uri\n\n";
+
+$pattern = "/\/api\/(?'scope'images)?\/?(?'id'\d+)?\/?(?'field'\w+)?/";
+$matches = [];
+preg_match($pattern, $uri, $matches);
+
+// пробую meta на вкус
+$meta = json_decode(file_get_contents('../data/meta.json'), true);
+$id = $matches['id'] ?? null;
+
+if ($id) {
+  var_dump($meta[$id]);
+} else if (!$id) {
+  var_dump($meta);
+}
