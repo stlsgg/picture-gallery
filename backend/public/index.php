d4aa1c921bfc31c546c7a14d7f303d20eeadc69c
stlsgg <gregory.guzey@gmail.com>
2025-11-09 11:35:43 +0300

    refactor: index refactor

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,54 +1,7 @@
 <?php
-require_once "../src/classes/Storage.php";
-require_once "../src/classes/Request.php";
-require_once "../src/classes/Response.php";
+require_once "../src/classes/Controller.php";
 
 $uri = $_SERVER["REQUEST_URI"];
 
-
-// open meta.json
-try {
-  $db = new Storage("../src/data/meta.json");
-} catch (Exception $err) {
-  Response::error(
-    code: 500,
-    message: "error occurred while trying to open database: $err: "
-      . $err->getMessage()
-  );
-}
-
-// open request
-try {
-  $req = new Request($uri);
-} catch (Exception $err) {
-  Response::error(
-    code: 400,
-    message: "error occurred while reading request: $err: "
-      . $err->getMessage()
-  );
-}
-
-// get response by request
-if ($req->isCollection()) {
-  Response::ok($db->readAll());
-}
-
-if ($req->isSingle()) {
-  $id = $req->getId();
-  $obj = $db->readById($id);
-  if (!$obj) Response::error(404, "object with id $id not found");
-  Response::ok($obj);
-}
-
-if ($req->isField()) {
-  $id = $req->getId();
-  $field = $req->getField();
-  $obj = $db->readById($id);
-  if (!$obj) Response::error(404, "object with id $id not found");
-  if (!array_key_exists($field, $obj)) Response::error(404, "field $field not found");
-  Response::ok($obj[$field]);
-}
-
-// fallback error
-Response::error(503, "unsupported error");
-
+$app = new Controller();
+$app->requestHandler();
