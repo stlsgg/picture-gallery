a532d3689de4f79f7e512654bcea61b4f3524287
stlsgg <gregory.guzey@gmail.com>
2025-11-09 18:29:33 +0300

    refactor(view): расширение возможностей renderState
    теперь функция принимает Запись вида <string,string>, где ключ описывает
    свойство переданного HTML элемента, а значение - новое значение для
    данного свойства объекта.

--- frontend/js/view/renderState.js
+++ frontend/js/view/renderState.js
@@ -3,9 +3,15 @@
  *
  * @param {HTMLElement} targetElement - Target element where the value will be
  * rendered.
- * @param {string|number} value - Value to insert into the element's innerHTML.
+ * @param {Record<string, string>} effects - A map of element property names
+ * and their new values (e.g., { innerText: "Hello", className: "active" }).
+ * Each key will be applied directly to the target element if it exists.
  * @returns {void}
  */
-export const renderState = (targetElement, value) => {
-  targetElement.innerHTML = value;
+export const renderState = (targetElement, effects) => {
+  for (const [key, val] of Object.entries(effects)) {
+    if (key in targetElement) {
+      targetElement[key] = val;
+    }
+  }
 };
