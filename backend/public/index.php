0988b4ceca2f50684dc79be1119208d37be6174c
stlsgg <gregory.guzey@gmail.com>
2025-11-11 01:48:04 +0300

    refactor: index.php как контроллер
    связал все воедино в index.php, который и принимает на себя запросы
    клиента. нужно будет допилить

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,7 +1,39 @@
 <?php
-require_once "../src/classes/Controller.php";
+// constants
+$rootPath = "/var/www/backend";
+$public = $_SERVER["DOCUMENT_ROOT"];
+$dbPath = "$rootPath/src/data/meta.json";
+$fullPath = "/upload/full/";
+$thumbPath = "/upload/thumbnails/";
+$classDir = "$rootPath/src/classes";
 
-$uri = $_SERVER["REQUEST_URI"];
+// подключение классов
+require_once "$classDir/Router.php";
+require_once "$classDir/Response.php";
+require_once "$classDir/Storage.php";
 
-$app = new Controller();
-$app->requestHandler();
+$router = new Router();
+$db = new Storage($dbPath);
+
+$router->on("get", "/check", function () {
+  Response::ok(200, "check success");
+});
+
+$router->on("get", "/images/{id:int}", function ($id) use ($db) {
+  Response::ok(200, $db->readById($id));
+});
+
+/* $router->on("delete", "/images/{id:int}", function ($id) use ($db) { */
+/*   $db->delete($id); */
+/*   Response::ok(204); */
+/* }); */
+
+/* $router->on("post", "/images", function () { */
+/*   Response::ok(201, "created"); */
+/* }); */
+
+$router->on("get", "/images", function () use ($db) {
+  Response::ok(200, $db->readAll());
+});
+
+$router->listen();
