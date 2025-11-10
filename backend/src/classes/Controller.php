31332d3a1584026016cd0d94b34b3da94afb1d69
stlsgg <gregory.guzey@gmail.com>
2025-11-11 01:27:35 +0300

    refactor: удаление requestHandler из Controller
    ответственность перенесена на Router

--- backend/src/classes/Controller.php
+++ backend/src/classes/Controller.php
@@ -3,7 +3,6 @@
 class Controller
 {
   private Storage $db;
-  private Request $request;
 
   public function __construct()
   {
@@ -17,15 +16,6 @@ class Controller
       Response::error(
         code: 500,
         message: "error occurred while trying to open database: $err: "
-          . $err->getMessage()
-      );
-    }
-    try {
-      $this->request = new Request();
-    } catch (Exception $err) {
-      Response::error(
-        code: 400,
-        message: "error occurred while reading request: $err: "
           . $err->getMessage()
       );
     }
@@ -81,45 +71,5 @@ class Controller
     // загрузка на сервер через FileManager
     FileManager::saveImage($thumbnail->getImage(), $rootPath . $thumbPath, $image->mimetype);
     FileManager::saveImage($imageWorker->getImage(), $rootPath . $fullPath, $image->mimetype);
-  }
-
-  // перехватывает запрос и вызывает нужный callback
-  public function requestHandler(): void
-  {
-    // POST разрешен только на /api/images
-    // GET разрешен на id, поля и /api/images
-    // get response by request
-    $method = $this->request->getMethod();
-    if ($method === "POST") {
-      $this->postImage();
-      Response::ok(["message" => "image uploaded"]);
-    } else if ($method === "GET") {
-      if ($this->request->check()) {
-        Response::ok("check success");
-      }
-
-      if ($this->request->isCollection()) {
-        Response::ok($this->db->readAll());
-      }
-
-      if ($this->request->isSingle()) {
-        $id = $this->request->getId();
-        $obj = $this->db->readById($id);
-        if (!$obj) Response::error(404, "object with id $id not found");
-        Response::ok($obj);
-      }
-
-      if ($this->request->isField()) {
-        $id = $this->request->getId();
-        $field = $this->request->getField();
-        $obj = $this->db->readById($id);
-        if (!$obj) Response::error(404, "object with id $id not found");
-        if (!array_key_exists($field, $obj)) Response::error(404, "field $field not found");
-        Response::ok($obj[$field]);
-      }
-    }
-
-    // fallback error
-    Response::error(503, "unsupported error");
   }
 }
