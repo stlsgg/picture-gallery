aadef006907a5535cd63dd0bc86f32b495f841e7
stlsgg <gregory.guzey@gmail.com>
2025-11-11 01:20:45 +0300

    refactor: Request стал static классом для быстрого доступа к переменным
    - без ответственностей: методы взял на себя Router
    - поля для доступа стали статическими для быстрого доступа
    - поля: uri, method, data, где data это копия $_POST данных

--- backend/src/classes/Request.php
+++ backend/src/classes/Request.php
@@ -1,95 +1,14 @@
 <?php
-// класс по парсингу и валидации URI по отношению к API
-class InvalidURIException extends Exception {};
-
 class Request
 {
-  private string $uri = "";
-  private string $method = "";
+  public static string $uri = "";
+  public static string $method = "";
+  public static array $data = [];
 
-  public function __construct(?string $uri= null)
+  public function __construct()
   {
-    $uri = $uri ?? $_SERVER["REQUEST_URI"];
-    $uri = rtrim($uri, "/");
-    $this->validate($uri);
-    $this->uri = $uri;
+    $this->uri = rtrim($_SERVER["REQUEST_URI"], "/");
     $this->method = $_SERVER['REQUEST_METHOD'];
-  }
-
-  // проверка валидности переданного uri
-  private function validate(string $uri)
-  {
-    if (!preg_match("#^/api/(images|check)(/(\d+(/\w+)?)?)?$#", $uri)) {
-      throw new InvalidURIException("invalid uri given: $uri");
-    }
-  }
-
-  // получить id
-  public function getId(): ?string
-  {
-    $m = [];
-    $pattern = "#^/api/images/(?P<id>\d+)(/|$)#";
-    if (preg_match($pattern, $this->uri, $m)) {
-      return $m['id'] ?? null;
-    }
-    return null;
-  }
-
-  // получить field
-  public function getField(): ?string
-  {
-    $m = [];
-    $pattern = "#^/api/images/\d+/(?P<field>\w+)(/|$)#";
-    if (preg_match($pattern, $this->uri, $m)) {
-      return $m['field'] ?? null;
-    }
-    return null;
-  }
-
-  // проверка на наличие id в запросе
-  public function hasId(): bool
-  {
-    $m = [];
-    $pattern = "#^/api/images/(?P<id>\d+)(/|$)#";
-    return preg_match($pattern, $this->uri, $m) ? true : false;
-  }
-
-  // проверка на наличие field в запросе
-  public function hasField(): bool
-  {
-    $m = [];
-    $pattern = "#^/api/images/\d+/(?P<field>\w+)(/|$)#";
-    return preg_match($pattern, $this->uri, $m) ? true : false;
-  }
-
-  // сценарий, где клиент запросил общую структуру по пути /api/images
-  public function isCollection(): bool
-  {
-    return !$this->hasId();
-  }
-
-  // сценарий, где клиент запросил информацию только об одной картинке
-  public function isSingle(): bool
-  {
-    return $this->hasId() && !$this->hasField();
-  }
-
-  // сценарий, где клиент запросил информацию о конкретном поле у конкретной
-  // картинки
-  public function isField(): bool
-  {
-    return $this->hasId() && $this->hasField();
-  }
-
-  // получить метод запроса
-  public function getMethod(): string {
-    return $this->method;
-  }
-
-  // check api connection
-  public function check(): bool {
-    $m = [];
-    $pattern = "#^/api/check(/|$)#";
-    return preg_match($pattern, $this->uri, $m) ? true : false;
+    $this->data = $_POST;
   }
 }
